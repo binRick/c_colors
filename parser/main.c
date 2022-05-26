2f22a14de842aaccf514f43a68e0b64bf3ee91c0
rick <rick@ricks-MacBook-Air.local>
2022-05-26 04:16:55 -0400

    functional color parser

--- parser/main.c
+++ parser/main.c
@@ -1,40 +1,123 @@
-#include "../include/colors.h"
-#include <assert.h>
-#include <ctype.h>
-#include <math.h>
-#include <stdarg.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdlib.h>
-#include <string.h>
-#include <string.h>
-#include <sys/time.h>
-int parse_csv();
+#include "parser.h"
 
-#define RENDER_COLOR_OUTPUT     false
-#define RENDER_PRETTY_JSON      true
-#define COLORNAMES_CSV_FILE     "vendor/colornames/colornames.csv"
-#define COLORNAMES_JSON_FILE    "vendor/colornames/colornames.json"
 
+void ansi_256_fg(FILE *file, int color) {
+    fprintf(file, C256_FG_CODE, color);
+}
+
+void ansi_256_bg(FILE *file, int color) {
+    fprintf(file, C256_BG_CODE, color);
+}
+
+void ansi_truecolor_fg(FILE *file, int r, int g, int b) {
+    fprintf(file, TRUECOLOR_FG_CODE, r, g, b);
+}
+
+void ansi_truecolor_bg(FILE *file, int r, int g, int b) {
+    fprintf(file, TRUECOLOR_BG_CODE, r, g, b);
+}
+
+void ansi_reset(FILE *file) {
+    fputs(RESET_CODE, file);
+}
+args_t args = {
+    DEFAULT_INPUT,
+    DEFAULT_OUTPUT,
+    DEFAULT_MODE,
+    DEFAULT_VERBOSE,
+    DEFAULT_COUNT,
+};
+
+
+int debug_args(){
+  fprintf(stderr,
+    acs(AC_BRIGHT_BLUE_BLACK AC_ITALIC  "Verbose: %d")
+    ", "
+    ansistr(AC_RESETALL AC_REVERSE AC_BRIGHT_RED_BLACK "Input: %s")
+    ", "
+    ansistr(AC_RESETALL AC_BRIGHT_GREEN_BLACK "Output: %s")
+    ", "
+    ansistr(AC_RESETALL AC_BRIGHT_GREEN_BLACK "Mode: %s")
+    ", "
+    ansistr(AC_RESETALL AC_UNDERLINE "Count: %d")
+    "\n"
+    ansistr(AC_RESETALL AC_UNDERLINE "Pretty Mode: %d")
+    "\n"
+    , args.verbose, args.input, args.output, args.mode, args.count, args.pretty
+    );
+
+  return(EXIT_SUCCESS);
+}
 
 int main(int argc, char **argv) {
   if ((argc >= 2) && (strcmp(argv[1], "--test") == 0)) {
     printf("Test OK\n"); return(0);
   }
-  parse_csv();
-  printf("Test OK\n"); return(0);
+  parse_args(argc, argv);
+  assert(debug_args() == 0);
+
+  
+  if ((strcmp(args.mode, "csv") == 0)) return(parse_csv());
+
+  printf(AC_RESETALL AC_RED "No mode selected: %s\n" AC_RESETALL, args.mode); return(0);
 
   return(EXIT_SUCCESS);
 }
 
 
-int parse_csv(){
-  char                   *content = fs_read(COLORNAMES_CSV_FILE);
-  struct StringFNStrings Lines    = stringfn_split_lines_and_trim(content);
 
-  printf("%d lines\n", Lines.count);
+int parse_args(int argc, char *argv[]){
+  char               identifier;
+  const char         *value;
+  cag_option_context context;
+
+  cag_option_prepare(&context, options, CAG_ARRAY_SIZE(options), argc, argv);
+  while (cag_option_fetch(&context)) {
+    identifier = cag_option_get(&context);
+    switch (identifier) {
+    case 'i':
+      value    = cag_option_get_value(&context);
+      args.input = value;
+      break;
+    case 'm':
+      value    = cag_option_get_value(&context);
+      args.mode = value;
+      break;
+    case 'o':
+      value    = cag_option_get_value(&context);
+      args.output = value;
+      break;
+    case 'p':
+      args.pretty = true;
+      break;
+    case 'C':
+      args.color = true;
+      break;
+    case 'v':
+      args.verbose = true;
+      break;
+    case 'c':
+      value    = cag_option_get_value(&context);
+      args.count = atoi(value);
+      break;
+    case 'h':
+      printf(AC_RESETALL AC_YELLOW AC_BOLD "Usage: cargsdemo [OPTION]...\n" AC_RESETALL);
+      printf("Demonstrates the cargs library.\n\n");
+      cag_option_print(options, CAG_ARRAY_SIZE(options), stdout);
+      printf("\nNote that all formatting is done by cargs.\n");
+      return(EXIT_SUCCESS);
+    }
+  }
+
+
+  return(EXIT_SUCCESS);
+}
+
+int parse_csv(){
+  char                   *content = fs_read(args.input);
+  struct StringFNStrings Lines    = stringfn_split_lines_and_trim(content);
+  if(args.verbose)
+      fprintf(stderr, "%d lines\n", Lines.count);
   int         qty = 0, ui;
 
   short       ok;
@@ -43,7 +126,7 @@ int parse_csv(){
   JSON_Value  *o;
   JSON_Object *O;
 
-  for (int i = 0; i < Lines.count; i++) {
+  for (int i = 0; i < Lines.count && ((i+1) < args.count); i++) {
     LINE = stringfn_trim(Lines.strings[i]);
     struct StringFNStrings SPLIT = stringfn_split(LINE, ',');
     free(LINE);
@@ -106,23 +189,23 @@ int parse_csv(){
     json_object_dotset_string(O, "seq.truecolor.fg", C->truecolor->fg);
     json_object_dotset_string(O, "seq.truecolor.bg", C->truecolor->bg);
     /////////////////////////////////////////////////////////////////////////
-    if (RENDER_PRETTY_JSON) {
+    if (args.pretty) {
       js = json_serialize_to_string_pretty(o);
     }else{
       js = json_serialize_to_string(o);
     }
-    if (RENDER_COLOR_OUTPUT) {
+    if (args.color) {
       ansi_truecolor_bg(stdout, C->rgb->red, C->rgb->green, C->rgb->blue);
     }
     fprintf(stdout, "%s\n", js);
-    if (RENDER_COLOR_OUTPUT) {
-      ansi_reset(stdout);
+    if (args.color) {
+      printf(AC_RESETALL);
     }
 
 
     qty++;
   }
-
   return(EXIT_SUCCESS);
-} /* csv_logic */
+}
+
 
