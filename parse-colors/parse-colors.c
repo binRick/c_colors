71dfe0d9c5d9a0cf694806705c1a18989df3be21
rick <rick@ricks-MacBook-Air.local>
2022-05-29 22:05:53 -0400

    functional paletta loader POC

--- parse-colors/parse-colors.c
+++ parse-colors/parse-colors.c
@@ -1,4 +1,13 @@
 #include "parse-colors.h"
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include "termpaint.h"
+#include "termpaintx.h"
+#include "termpaintx_ttyrescue.h"
+#define streq(a,b) (strcmp(a,b)==0)
+#define DEBUG_TERMINAL_CAPABILITIES false
 args_t args = {
   DEFAULT_CSV_INPUT,
   DEFAULT_OUTPUT,
@@ -7,15 +16,202 @@ args_t args = {
   DEFAULT_COUNT,
   DEFAULT_COLOR,
 };
+
+typedef struct {
+    bool RestorePalette;
+} TerminalCapabilities_t;
+
+TerminalCapabilities_t TerminalCapabilities = {
+    RestorePalette: false,
+};
+
+
+
 void cleanup();
 void restore_screen();
 void setup_screen();
 void load_new_palette_type_id();
 
 
+
+
+typedef enum {
+  OFF,
+  ON,
+  QUERY,
+} BoolQuery;
+
+static void await_c1(unsigned char c1)
+{
+  unsigned char c;
+
+  /* await CSI - 8bit or 2byte 7bit form */
+  bool in_esc = false;
+  while((c = getchar())) {
+    if(c == c1)
+      break;
+    if(in_esc && c == (char)(c1 - 0x40))
+      break;
+    if(!in_esc && c == 0x1b)
+      in_esc = true;
+    else
+      in_esc = false;
+  }
+}
+static char *read_csi()
+{
+  await_c1(0x9B); // CSI
+
+  /* TODO: This really should be a more robust CSI parser
+   */
+  char csi[32];
+  int i = 0;
+  for(; i < sizeof(csi)-1; i++) {
+    char c = csi[i] = getchar();
+    if(c >= 0x40 && c <= 0x7e)
+      break;
+  }
+  csi[++i] = 0;
+
+  // TODO: returns longer than 32?
+
+  return strdup(csi);
+}
+static bool query_dec_mode(int mode)
+{
+  printf("\x1b[?%d$p", mode);
+
+  char *s = NULL;
+  do {
+    if(s)
+      free(s);
+    s = read_csi();
+
+    /* expect "?" mode ";" value "$y" */
+
+    int reply_mode, reply_value;
+    char reply_cmd;
+    /* If the sscanf format string ends in a literal, we can't tell from
+     * its return value if it matches. Hence we'll %c the cmd and check it
+     * explicitly
+     */
+    if(sscanf(s, "?%d;%d$%c", &reply_mode, &reply_value, &reply_cmd) < 3)
+      continue;
+    if(reply_cmd != 'y')
+      continue;
+
+    if(reply_mode != mode)
+      continue;
+
+    free(s);
+
+    if(reply_value == 1 || reply_value == 3)
+      return true;
+    if(reply_value == 2 || reply_value == 4)
+      return false;
+
+    printf("Unrecognised reply to DECRQM: %d\n", reply_value);
+    return false;
+  } while(1);
+}
+static void do_dec_mode(int mode, BoolQuery val, const char *name){
+  switch(val) {
+    case OFF:
+    case ON:
+      printf("\x1b[?%d%c", mode, val == ON ? 'h' : 'l');
+      break;
+
+    case QUERY:
+      if(query_dec_mode(mode))
+        printf("%s on\n", name);
+      else
+        printf("%s off\n", name);
+      break;
+  }
+}
+
+char *strdup_escaped(const char *tmp) {
+  // escaping could quadruple size
+  char *ret = malloc(strlen(tmp) * 4 + 1);
+  char *dst = ret;
+
+  for ( ; *tmp; tmp++) {
+    if (*tmp >= ' ' && *tmp <= 126 && *tmp != '\\') {
+      *dst = *tmp;
+      ++dst;
+    } else {
+      dst += sprintf(dst, "\\x%02hhx", (unsigned char)*tmp);
+    }
+  }
+  *dst = 0;
+  return(ret);
+}
+
+
+void null_callback(void *ctx, termpaint_event *event) {
+  (void)ctx; (void)event;
+}
+
+typedef struct {
+  int        id;
+  const char *name;
+  _Bool      state;
+} Cap;
+
+Cap  caps[] = {
+#define C(name) { TERMPAINT_CAPABILITY_ ## name, #name, 0 }
+  C(CSI_POSTFIX_MOD),
+  C(TITLE_RESTORE),
+  C(MAY_TRY_CURSOR_SHAPE_BAR),
+  C(CURSOR_SHAPE_OSC50),
+  C(EXTENDED_CHARSET),
+  C(TRUECOLOR_MAYBE_SUPPORTED),
+  C(TRUECOLOR_SUPPORTED),
+  C(88_COLOR),
+  C(CLEARED_COLORING),
+  C(7BIT_ST),
+  C(MAY_TRY_TAGGED_PASTE),
+  C(TRUECOLOR_SUPPORTED),
+  C(SAFE_POSITION_REPORT),
+  C(EXTENDED_CHARSET),
+#undef C
+  { 0,                             NULL,  0 }
+};
+
+char *debug     = NULL;
+bool debug_used = true;
+
+
+void debug_log(termpaint_integration *integration, const char *data, int length) {
+  (void)integration;
+  if (debug_used && !debug) {
+    return;                           // memory allocaton failure
+  }
+  if (debug) {
+    const int oldlen     = strlen(debug);
+    char      *debug_old = debug;
+    debug = realloc(debug, oldlen + length + 1);
+    if (debug) {
+      memcpy(debug + oldlen, data, length);
+      debug[oldlen + length] = 0;
+    } else {
+      free(debug_old);
+    }
+  } else {
+    debug = strndup(data, length);
+  }
+  debug_used = true;
+}
+
+
 void setup_screen(){
+  do_dec_mode(1049, 2, "altscreen");
+
+  sleep(5);
+
   printf(AC_ALT_SCREEN_ON);
-  printf(AC_SAVE_PALETTE);
+  if(TerminalCapabilities.RestorePalette)
+    printf(AC_SAVE_PALETTE);
   printf(AC_HIDE_CURSOR);
   signal(SIGTERM, restore_screen);
   signal(SIGQUIT, restore_screen);
@@ -27,7 +223,8 @@ void setup_screen(){
 void restore_screen(){
   printf(AC_SHOW_CURSOR);
   printf(AC_ALT_SCREEN_OFF);
-  printf(AC_RESTORE_PALETTE);
+  if(TerminalCapabilities.RestorePalette)
+      printf(AC_RESTORE_PALETTE);
   cleanup();
 }
 
@@ -42,7 +239,74 @@ void cleanup(){
 }
 
 
+int QUERY_TERMINAL(){
+  termpaint_integration *integration = termpaintx_full_integration("+kbdsigint +kbdsigtstp");
+
+  if (!integration) {
+    puts("Could not init!");
+    return(1);
+  }
+  termpaint_integration_set_logging_func(integration, debug_log);
+  termpaint_terminal *terminal = termpaint_terminal_new(integration);
+
+  termpaint_terminal_set_log_mask(terminal, TERMPAINT_LOG_AUTO_DETECT_TRACE | TERMPAINT_LOG_TRACE_RAW_INPUT);
+  termpaint_terminal_set_event_cb(terminal, null_callback, NULL);
+  termpaintx_full_integration_set_terminal(integration, terminal);
+  termpaint_terminal_auto_detect(terminal);
+  termpaintx_full_integration_wait_for_ready_with_message(integration, 3000, "Terminal auto detection is taking unusually long, press space to abort.");
+
+  char buff[1000];
+  char *self_reported_name_and_version = NULL;
+
+  termpaint_terminal_auto_detect_result_text(terminal, buff, sizeof(buff));
+  if (termpaint_terminal_self_reported_name_and_version(terminal)) {
+    self_reported_name_and_version = strdup_escaped(termpaint_terminal_self_reported_name_and_version(terminal));
+  }
+  termpaint_terminal_free_with_restore(terminal);
+  for (Cap *c = caps; c->name; c++) {
+    c->state = termpaint_terminal_capable(terminal, c->id);
+    if(strcmp(c->name,"TITLE_RESTORE")==0)
+        TerminalCapabilities.RestorePalette = c->state;
+    if(DEBUG_TERMINAL_CAPABILITIES)
+        printf(AC_RESETALL AC_YELLOW AC_ITALIC "%30s" AC_RESETALL 
+            " => " 
+            "[" AC_REVERSED AC_BOLD "%3s" AC_RESETALL "]"
+            "\n"
+            , c->name
+            , c->state ? AC_GREEN "Yes" : AC_RED "No"
+            );
+  }
+  if(DEBUG_TERMINAL_CAPABILITIES || args.verbose){
+      printf(AC_RED AC_BOLD "%s" AC_RESETALL "\n", self_reported_name_and_version);
+      printf(AC_RED AC_BOLD "%s" AC_RESETALL "\n", buff);
+      printf(AC_BLUE AC_BOLD "Restore Palette?" AC_RESETALL " " "%s" AC_RESETALL "\n", TerminalCapabilities.RestorePalette ? AC_GREEN "Yes" : AC_RED "No");
+//      printf("\e[?1049%c", "
+      sleep(5);
+  }
+  return(EXIT_SUCCESS);
+}
+
+
+char *draw_box(int BOX_SIZE){
+  int  W = BOX_SIZE * 2, H = BOX_SIZE;
+  char *out = malloc(1024);
+  char *BG  = AC_RESETALL AC_BLACK "" AC_BG24(255, 255, 255);
+
+  sprintf(out, "%s", BG);
+  for (int y = 0; y < H; y++) {
+    for (int x = 0; x < W; x++) {
+      strcat(out, " ");
+    }
+    strcat(out, "\n");
+  }
+  return(out);
+}
+
+
 void load_new_palette_type_id(int PALETTE_TYPE_ID){
+  if(!TerminalCapabilities.RestorePalette)
+        return;
+
   struct StringFNStrings p = stringfn_split(WorkerPaletteTypes[PALETTE_TYPE_ID].Colors, ' ');
 
   fprintf(stdout, "Loading %s Colors\n", WorkerPaletteTypes[PALETTE_TYPE_ID].Name);
@@ -71,6 +335,10 @@ void load_new_palette_type_id(int PALETTE_TYPE_ID){
           );
 
   printf("%s\n", new_palette_codes);
+  char *box = draw_box(5);
+
+  printf(AC_RESETALL "%s" AC_RESETALL "\n", box);
+  free(box);
   free(new_palette_codes);
 }
 
@@ -83,21 +351,21 @@ void print_color_name_handler(ParsedColor *PARSED_COLOR_ITEM){
 
 
 int main(int argc, char **argv) {
-  setup_screen();
-  load_new_palette_type_id(0);
   parse_args(argc, argv);
   if ((argc >= 2) && (strcmp(argv[1], "--test") == 0)) {
     printf("Test OK\n"); return(0);
   }
+  QUERY_TERMINAL();
+  setup_screen();
+  load_new_palette_type_id(DEFAULT_PALETTE);
 
   if ((strcmp(args.mode, "debug_args") == 0)) {
-    load_new_palette_type_id(4);
-
+    load_new_palette_type_id(ARGS_PALETTE);
     return(debug_args());
   }
 
   if ((strcmp(args.mode, "csv") == 0)) {
-    load_new_palette_type_id(1);
+    load_new_palette_type_id(CSV_PALETTE);
     parse_csv_options *options = malloc(sizeof(parse_csv_options));
     options->input_file       = args.input;
     options->verbose_mode     = args.verbose;
@@ -107,7 +375,7 @@ int main(int argc, char **argv) {
     return(parse_colors_csv(options));
   }
   if ((strcmp(args.mode, "db") == 0)) {
-    load_new_palette_type_id(3);
+    load_new_palette_type_id(DB_PALETTE);
     ColorsDB *DB = malloc(sizeof(ColorsDB));
     DB->Path = COLOR_NAMES_DB_PATH;
     return(db_list_ids(DB));
--- parse-colors/parse-colors.h
+++ parse-colors/parse-colors.h
@@ -1,6 +1,10 @@
 #pragma once
 #ifndef PARSER_H
 #define PARSER_H
+#ifndef AC_SHOW_CURSOR
+#define AC_SHOW_CURSOR                 "\x1b[?25h"
+#endif
+////////////////////////////////////////////////////////////////////
 #include <ctype.h>
 #include <math.h>
 #include <stdarg.h>
@@ -10,8 +14,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
-
-
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
 #include "../colors-csv-parser/colors-csv-parser.h"
 #include "../colors-json-parser/colors-json-parser.h"
 #include "../db/db.h"
@@ -19,11 +23,7 @@
 #include "../submodules/c_ansi/ansi-codes/ansi-codes.h"
 #include "../submodules/c_ansi/ansi-rgb-utils/ansi-rgb-utils.h"
 #include "../subprojects/cargs/include/cargs.h"
-//#include "../submodules/c_timer/include/c_timer.h"
-//#ifndef DBG_H_c49aa87c_54eb_46d2_8d72_a51f5efce1ac
-//#include "../submodules/dbg.h/dbg.h"
-//#endif
-
+////////////////////////////////////////////////////////////////////
 #define RENDER_COLOR_OUTPUT  false
 #define RENDER_PRETTY_JSON   alse
 #define COLORNAMES_CSV_FILE  "vendor/colornames/colornames.csv"
@@ -38,14 +38,14 @@
 #define DEFAULT_PRETTY       false
 #define DEFAULT_COLOR        false
 #define DEFAULT_MODE         "csv"
-////////////////////////////////////////////////////////////////////
 #define VERBOSE_DEBUG_HANDLER false
-
+////////////////////////////////////////////////////////////////////
 int parse_args(int argc, char *argv[]);
 int debug_args();
 int hex_to_closest_ansi_code(const uint32_t trp);
 int parse_csv();
 
+////////////////////////////////////////////////////////////////////
 typedef struct CLI_ARGS {
   char *input;
   char *output;
@@ -55,7 +55,6 @@ typedef struct CLI_ARGS {
   bool pretty;
   int  color;
 } args_t;
-
 static struct cag_option options[] = {
   { .identifier     = 'i',
     .access_letters = "i",
@@ -144,16 +143,14 @@ struct WorkerPaletteType_t {
   char *Name;
   char *Colors;
 };
-
-
-#ifndef AC_SHOW_CURSOR
-#define AC_SHOW_CURSOR                 "\x1b[?25h"
-#endif
+//➜  evtm git:(master) P=~/.palettes/harper-dark
+//➜  evtm git:(master) cat  $P|tr '[a-z]' '[A-Z]'|cut -d= -f2|tr '\n' ' '|pbcopy
 #define DEBUG_COLORS "EBEEF9 0D101B EBEEF9 000009 BB4F6C 72CCAE C65E3D 58B6CA 6488C4 8D84C6 858893 1F222D EE829F A5FFE1 F99170 8BE9FD 97BBF7 C0B7F9 FFFFFF"
 #define CSV_COLORS "D9D8D8 231F20 D9D8D8 231F20 EE2E24 00853E FFD204 009DDC 98005D 85CEBC D9D8D8 737171 EE2E24 00853E FFD204 009DDC 98005D 85CEBC FFFFFF"
 #define DB_COLORS "F2F2F2 041A3B F2F2F2 303030 E1321A 6AB017 FFC005 729FCF EC0048 2AA7E7 F2F2F2 5D5D5D FF361E 7BC91F FFD00A 0071FF FF1D62 4BB8FD A020F0"
 #define JSON_COLORS "D0D0D0 1C1C1C D0D0D0 2F2E2D A36666 90A57D D7AF87 7FA5BD C79EC4 8ADBB4 D0D0D0 4A4845 D78787 AFBEA2 E4C9AF A1BDCE D7BEDA B1E7DD EFEFEF"
 #define DEFAULT_COLORS "D0D0D0 000000 D0D0D0 000000 FF0000 33FF00 FF0099 0066FF CC00FF 00FFFF D0D0D0 808080 FF0000 33FF00 FF0099 0066FF CC00FF 00FFFF FFFFFF"
+#define ARGS_COLORS "A8A49D 010101 A8A49D 010101 F8B63F 7FB5E1 D6DA25 489E48 B296C6 F5BFD7 A8A49D 726E6A F8B63F 7FB5E1 D6DA25 489E48 B296C6 F5BFD7 FEFBEA"
 
 static WorkerPaletteType_t WorkerPaletteTypes[] = {
   { .Name = "DEFAULT_COLORS", .Colors = DEFAULT_COLORS },
@@ -161,7 +158,14 @@ static WorkerPaletteType_t WorkerPaletteTypes[] = {
   { .Name = "JSON",           .Colors = JSON_COLORS    },
   { .Name = "DB",             .Colors = DB_COLORS      },
   { .Name = "DEBUG",          .Colors = DEBUG_COLORS   },
+  { .Name = "ARGS",           .Colors = ARGS_COLORS    },
 };
+enum { DEFAULT_PALETTE,
+       CSV_PALETTE,
+       JSON_PALETTE,
+       DB_PALETTE,
+       DEBUG_PALETTE,
+       ARGS_PALETTE };
 
 
 #endif
